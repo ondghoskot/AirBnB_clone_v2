3bd3c884019838f9784a7c817be7d32a58312c30
ondghoskot <anaschtioui65@gmail.com>
2024-04-20 22:31:26 +0000

    new method + ipdated all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a dictionary of objects of one type of class"""
+        if cls is None:
+            return FileStorage.__objects
+        opfilter = {}
+        for key, value in FileStorage.__objects.items():
+            if key.split('.')[0] == cls.__name__:
+                opfilter[key] = value
+        return opfilter
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -53,6 +59,7 @@ class FileStorage:
         """delete obj from __objects if it’s inside"""
         if obj:
             k = obj.__class__.__name__ + '.' + obj.id
-            if k in self.__objects:
-                del self.__objects[key]
+            if k in FileStorage.__objects:
+                del FileStorage.__objects[k]
+                self.save()
         return
