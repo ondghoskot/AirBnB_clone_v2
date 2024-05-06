a9d765f679d54decd9b88f14933c23078a5be40f
anassch <anaschtioui65@gmail.com>
2024-05-06 19:06:50 +0000

    deploy web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# sets up web servers for deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo ufw enable
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo 'test' > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
+server {
+    listen 80;
+    listen [::]:80;
+    server_name anassch.tech;
+
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        internal;
+        alias /var/www/html/404.html;
+    }
+}
+EOF
+sudo service nginx restart
